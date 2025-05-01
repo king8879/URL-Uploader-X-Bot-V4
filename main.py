f4634d19b42759b9daffc4c64b366601a8b3aeee
Satyajeetkumarofficial <152394869+Satyajeetkumarofficial@users.noreply.github.com>
2025-05-01 10:49:33 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, CallbackQueryHandler
+from handlers.start import start
+from handlers.url_handler import handle_url
+from handlers.settings_handler import settings, button_handler
+from handlers.thumbnail import save_thumbnail
+from handlers.rename import handle_rename
+
+app = ApplicationBuilder().token("YOUR_BOT_TOKEN").build()
+
+app.add_handler(CommandHandler("start", start))
+app.add_handler(CommandHandler("settings", settings))
+app.add_handler(MessageHandler(filters.PHOTO, save_thumbnail))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_url))
+app.add_handler(CallbackQueryHandler(button_handler))
+app.add_handler(MessageHandler(filters.TEXT & filters.Regex(r'.+\..+'), handle_rename))
+
+app.run_polling()
